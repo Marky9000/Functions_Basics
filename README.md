794eb2cd661df3c51a1c52443bf5b4924bc2ef70
Mark van Booren <markvanbooren@hotmail>
2021-02-16 15:40:43 +0100

    substract

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Functions_Basics
+
+serie of steps have name
+functions are a box-machine
+
+Mental model of a function
+reCAll the function
+the Name is very important (for reuse)
---
+++ script.js
@@ -0,0 +1,41 @@
+const makePizza = function(){
+console.log("mix flour and water");
+console.log("heat the oven");
+console.log("Make pizzasaus");
+console.log("make round bottom");
+console.log("put saus on pizza bottem");
+console.log("add toppings");
+console.log("heat for 13 minutes");
+console.log("serve!");
+console.log("------------------------------------")
+
+}
+
+
+const makeSushi = function(){
+    console.log("boilsushirice");
+    console.log("lay seeweed");
+    console.log("put stickyrise on weed");
+    console.log("put filling on rise");
+    console.log("add raw fish");
+    console.log("roll with sushimap together");
+    console.log("slice");
+    console.log("serve with wasabi");
+    console.log("ginger & soya");
+    console.log("----------------------------------");
+
+  
+  
+}
+
+const makeThreePizzasAndTwoSushi = function (){
+    makePizza();
+    makePizza();
+    makePizza();
+    makeSushi();
+    makeSushi();
+
+}
+
+makeThreePizzasAndTwoSushi();
+
